79180c4a823a4531f1cb6d711fa80cd78e7693bd
Aahome <azizhanxk@gmail.com>
2025-04-09 21:32:07 +0700

    te

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,18 +8,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   let myObj = {
-    name: 'Aziz'
-    age: '1000'
+    name: 'Aziz',
+    age: '1000',
     address: {
-      city: 'Batam'
-      state: 'Riau'
+      city: 'Batam',
+      state: 'Riau',
       country: 'Indonesia'
     }
   }
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
-      <Card username="aziz" myArr={[1,2,3]}/>
+      <Card username="aziz" myArr={newArr}  />
       <Card username="Ahmd" post="Staff Engg."/>
       <Card />
     </>
